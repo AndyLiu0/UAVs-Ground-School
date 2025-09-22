53800b8da0405e5a5d88e5077350f15b9bd44d9d
AndyLiu0 <aliu3@student.smusd.us>
2025-09-22 08:40:29 -0700

    working stitch

--- week2/stitch.py
+++ week2/stitch.py
@@ -4,3 +4,25 @@ import numpy as np
 vid_path = "Minecraft_stitch_test.mp4"
 
 vcap = cv.VideoCapture(vid_path)
+
+def sift_all():
+    stitcher = cv.Stitcher.create(cv.Stitcher_SCANS)
+    frames = []
+    s, f = vcap.read()
+    i = 0
+    while s:
+        if not i%20: frames.append(f); #cv.imwrite(f"img{i}.png",f)
+        s, f = vcap.read()
+        #print(i := i+1)
+    print(f"stitching {len(frames)} frames")
+    status,out = stitcher.stitch(frames)
+    print("done")
+    if status == cv.Stitcher_OK:
+        cv.imshow("out",out)
+        cv.imwrite("stitch.png",out)
+        cv.waitKey(0)
+    else:
+        print("error")
+        sys.exit(-1)
+
+sift_all()
--- week2/test.py
+++ week2/test.py
@@ -1,3 +1,9 @@
-import cv2
+import cv2 as cv
 import numpy as np
-print(cv2.__version__)
+
+print(cv.__version__)
+img = cv.imread("Figure2Submission.png")
+cv.imshow("test",img)
+k = cv.waitKey(0)
+if k:
+    print("winclose")
